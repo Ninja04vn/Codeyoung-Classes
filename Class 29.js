e7c13788ee294806d155194607313151bd091914
Ninja04vn <136109539+Ninja04vn@users.noreply.github.com>
2023-06-09 17:55:53 +0100

    Add files via upload

---
+++ Class 29.js
@@ -0,0 +1,4 @@
+function MyFunction(){
+document.getElementById("Shriyan").innerHTML=
+"The sign of intelligence is that you are constantly wondering. Idiots are always dead sure about every damn thing they are doing in their life. The most beautiful moments in life are moments when you are expressing your joy, not when you are seeking it. If you resist change, you resist life"
+}
